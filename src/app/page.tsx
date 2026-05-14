40e86b813989c2683e8bd15ac1cf8f8d6789800a
Leonardo Emanuel <leonardoemanuel156@gmail.com>
2026-05-14 04:47:40 +0000

    useAuth must be used within an AuthProvider
    src/contexts/AuthContext.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 "use client"
 
 import React from 'react';
-import { AuthProvider } from '@/contexts/AuthContext';
 import DashboardLayout from './(dashboard)/layout';
 import Dashboard from './(dashboard)/page';
 
 export default function Home() {
   return (
-    <AuthProvider>
-      <DashboardLayout>
-        <Dashboard />
-      </DashboardLayout>
-    </AuthProvider>
+    <DashboardLayout>
+      <Dashboard />
+    </DashboardLayout>
   );
 }
