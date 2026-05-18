e50057067a6953a2ac86c6e22290b21c87985b51
leonardoemanuel156 <leonardoemanuel156@gmail.com>
2026-05-18 07:26:42 +0000

    removed gps logic

--- android/app/src/main/java/com/nocorre/app/MainActivity.java
+++ android/app/src/main/java/com/nocorre/app/MainActivity.java
@@ -4,15 +4,11 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.nocorre.app.gps.GPSPlugin;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Register the GPSPlugin
-        registerPlugin(GPSPlugin.class);
 
         // Start the foreground service
         Intent serviceIntent = new Intent(this, MyForegroundService.class);
