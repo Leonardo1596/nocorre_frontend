15d5356456087593fa964738d95e1e6790d68850
leonardoemanuel156 <leonardoemanuel156@gmail.com>
2026-06-02 03:42:24 +0000

    fixed MainActivity.java

--- android/app/src/main/java/com/nocorre/app/MainActivity.java
+++ android/app/src/main/java/com/nocorre/app/MainActivity.java
@@ -1,20 +1,5 @@
 package com.nocorre.app;
 
-import android.os.Bundle;
-
 import com.getcapacitor.BridgeActivity;
-import com.nocorre.app.gps.NativeGpsPlugin;
 
-import java.util.ArrayList;
-
-public class MainActivity extends BridgeActivity {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        registerPlugins(new ArrayList<Class<? extends com.getcapacitor.Plugin>>() {{ 
-            add(NativeGpsPlugin.class);
-        }});
-    }
-}
+public class MainActivity extends BridgeActivity {}
